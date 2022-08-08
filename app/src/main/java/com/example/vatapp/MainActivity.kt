9d8429ea4d0db2231e8f76e47394489c0d6cea38
FaisalMS <fis445@hotmail.com>
2022-08-08 11:23:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vatapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.vatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vatapp.databinding.ActivityMainBinding
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        binding.btnCalculateVat.setOnClickListener {
+            calculateVat()
+        }
+    }
+    private fun calculateVat() {
+        TODO("Not yet implemented")
+    }
+}
